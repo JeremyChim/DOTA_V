8c209dfe63746ea779a1c36dfa251ef23c6074aa
JeremyChim <jer888chim@outlook.com>
2023-07-30 05:28:39 +0800

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,3 +1,3 @@
 vpk.exe pak01_dir
-move "pak01_dir.vpk" "E:\Library\Steam\steamapps\common\dota 2 beta\game\0test"
+move "pak01_dir.vpk" "E:\GamePlatform\Steam\steamapps\common\dota 2 beta\game\0test"
 @pause
